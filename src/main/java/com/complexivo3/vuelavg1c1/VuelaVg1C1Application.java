efbcfe2f77b758cd023a353586e832d8ce0e4c49
JoseCedeño <81100807+Jose-22-ced@users.noreply.github.com>
2022-05-30 23:11:20 -0500

    Initial commit

---
+++ src/main/java/com/complexivo3/vuelavg1c1/VuelaVg1C1Application.java
@@ -0,0 +1,13 @@
+package com.complexivo3.vuelavg1c1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelaVg1C1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VuelaVg1C1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/complexivo3/vuelavg1c1/VuelaVg1C1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complexivo3.vuelavg1c1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuelaVg1C1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
